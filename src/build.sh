8c70761f3ed299932e851d7982af6c5d71c5470c
Roland Haas <rhaas@illinois.edu>
2021-10-22 09:55:06 -0500

    NSIMD: update tar file to nsimd 3.0.1

--- src/build.sh
+++ src/build.sh
@@ -14,7 +14,7 @@ set -e                          # Abort on errors
 
 # Set locations
 THORN=NSIMD
-NAME=nsimd-2.2
+NAME=nsimd-3.0.1
 SRCDIR="$(dirname $0)"
 BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
 if [ -z "${NSIMD_INSTALL_DIR}" ]; then
